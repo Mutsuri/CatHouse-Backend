6d12b9e9945d248be126ed358255339dec735124
Mutto Watson <pprsk@hotmail.com>
2023-08-19 01:27:05 +0700

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { User as UserModel} from '@prisma/client';
 
-@Controller()
+@Controller('user')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
-  getHello(): string {
+  getHello(): any {
     return this.appService.getHello();
   }
+
+  @Post('data')
+  async signupUser(
+    @Body() userData: { name?: string; email: string; birthYear?: number}
+  ): Promise<UserModel> {
+    return this.appService.createUser(userData);
+  }
 }
+
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getHello(): any {
     return 'You Come To Backend!';
+  }
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(userData): Promise<User>  
+  {    
+      const result = await this.prisma.user.create({data:      
+      userData});
+      return result;
   }
 }
