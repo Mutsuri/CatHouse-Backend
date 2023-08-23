3efd5b7347ed4942457a4e6b8cde452c3f89be2a
Mutto Watson <pprsk@hotmail.com>
2023-08-23 16:01:17 +0700

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { GoogleStrategy } from './google.strategy'
+
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, GoogleStrategy],
   exports: [PrismaService],
 })
 export class AppModule {}
