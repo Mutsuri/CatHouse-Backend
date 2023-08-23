961be684b0ced3affd6ebebdca4a3e85f03cb144
Mutto Watson <pprsk@hotmail.com>
2023-08-23 15:57:54 +0700

    init

---
+++ src/google.strategy.ts
@@ -0,0 +1,32 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { config } from 'dotenv';
+
+import { Injectable } from '@nestjs/common';
+
+config();
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+
+  constructor() {
+    super({
+      clientID: '1015115018764-prliuo2mdrbg9a9nen0tsdbk3lq159ru.apps.googleusercontent.com',
+      clientSecret: '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',
+      callbackURL: 'http://localhost:3000/google/redirect',
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate (accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+    const { name, emails, photos } = profile
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken
+    }
+    done(null, user);
+  }
+}
